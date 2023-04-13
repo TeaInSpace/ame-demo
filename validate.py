0f1027d1480bf941b04e25425fae0a12ac986153
Jessie Chatham Spencer <jessie@teainspace.com>
2023-04-13 21:05:30 +0200

    Fetch actual metrics from MLflow

--- validate.py
+++ validate.py
@@ -1,5 +1,12 @@
 import sys
+import mlflow
+from mlflow.entities import ViewType
 
-print("looks bad to me!")
 
-sys.exit(1)
+results = mlflow.search_registered_models(filter_string="name='logreg'")
+run_id = results[0].latest_versions[-1].run_id
+run = mlflow.get_run(run_id)
+print(run.data.metrics['score'])
+
+if run.data.metrics['score'] < 0.6:
+    sys.exit(1)
